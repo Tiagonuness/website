0bf95d50fdfad6293bcb3ddac061b6f5989cec5a
Tiago <tiagonunesyou18@gmail.com>
2024-03-10 02:19:10 -0300

    navbar em 95%

--- src/header/navbar.jsx
+++ src/header/navbar.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsList } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { logoTexto } from "../infos";
 import './navbar.css'
 
 function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <header className="sla">
+        <header className="navbar">
             <div>
-                
+                <nav>
+                    <p className="titulo">{logoTexto}</p>
+                    <div onClick={toggleMenu} className="list">
+                        <BsList/>
+                    </div>
+                    {menuOpen && (
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/contact">Contact</a></li>
+                        </ul>
+                    )}
+                </nav>
             </div>
         </header>
     )
